de3cef2e5d0eb9fdf32ee48f624385cd0027df4f
lordoverlord0 <rofyorug@gmail.com>
2021-04-16 02:51:52 +0300

    Bug fixes

--- Jubi/Abstracts/Executors/StrictCommandExecutor.cs
+++ Jubi/Abstracts/Executors/StrictCommandExecutor.cs
@@ -4,31 +4,31 @@ namespace Jubi.Abstracts.Executors
 {
     public abstract class CommandExecutor<T> : CommandExecutor
     {
-        public override Type[] PatternArgs { get; set; } = { typeof(T) };
+        protected override Type[] PatternArgs { get; } = { typeof(T) };
     }
     
     public abstract class CommandExecutor<T, T2> : CommandExecutor
     {
-        public override Type[] PatternArgs { get; set; } = { typeof(T), typeof(T2) };
+        protected override Type[] PatternArgs { get; } = { typeof(T), typeof(T2) };
     }
     
     public abstract class CommandExecutor<T, T2, T3> : CommandExecutor
     {
-        public override Type[] PatternArgs { get; set; } = { typeof(T), typeof(T2), typeof(T3) };
+        protected override Type[] PatternArgs { get; } = { typeof(T), typeof(T2), typeof(T3) };
     }
     
     public abstract class CommandExecutor<T, T2, T3, T4> : CommandExecutor
     {
-        public override Type[] PatternArgs { get; set; } = { typeof(T), typeof(T2), typeof(T3), typeof(T4) };
+        protected override Type[] PatternArgs { get; } = { typeof(T), typeof(T2), typeof(T3), typeof(T4) };
     }
     
     public abstract class CommandExecutor<T, T2, T3, T4, T5> : CommandExecutor
     {
-        public override Type[] PatternArgs { get; set; } = { typeof(T), typeof(T2), typeof(T3), typeof(T4), typeof(T5) };
+        protected override Type[] PatternArgs { get; } = { typeof(T), typeof(T2), typeof(T3), typeof(T4), typeof(T5) };
     }
     
     public abstract class CommandExecutor<T, T2, T3, T4, T5, T6> : CommandExecutor
     {
-        public override Type[] PatternArgs { get; set; } = { typeof(T), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6) };
+        protected override Type[] PatternArgs { get; } = { typeof(T), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6) };
     }
 }
